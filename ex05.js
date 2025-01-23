1a7300e1674a3f970e73a2df2eeae99618a14ea3
qwyx <mc010116@gmail.com>
2025-01-23 11:00:31 +0100

    ameliorate 05

--- ex05.js
+++ ex05.js
@@ -1,12 +1,16 @@
 function compterVoyelles(chaine) {
-    const voyelles = ["a", "e", "i", "o", "u", "y"]
-    let count = 0
-    for (let letter of chaine) {
-        if (voyelles.includes(letter)) {
-            count ++
+    let voyelles = ["a", "e", "i", "o", "u", "y"];
+    let count = 0;
+    for (let i = 0; i < chaine.length; i++) {
+        if (voyelles.includes(chaine[i])) {
+            count++;
         }
     }
-    return count
+    return count;
 }
 
 console.log(compterVoyelles("bonjour")); // Résultat attendu : 3
+console.log(compterVoyelles("salut")); // Résultat attendu : 2
+console.log(compterVoyelles("tableau")); // Résultat attendu : 4
+console.log(compterVoyelles("document")); // Résultat attendu : 3
+console.log(compterVoyelles("javascript")); // Résultat attendu : 3
