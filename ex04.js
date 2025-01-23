d13ebec3f37e8f1320d6f949dde41650cb1eccc0
qwyx <mc010116@gmail.com>
2025-01-23 10:57:58 +0100

    ameliorate 04

--- ex04.js
+++ ex04.js
@@ -1,10 +1,13 @@
 function dansPlage(nombre, min, max) {
-    if (nombre > min && nombre < max) {
-        return true
+    if (nombre >= min && nombre <= max) {
+        return true;
     } else {
-        return false
+        return false;
     }
 }
 
 console.log(dansPlage(5, 1, 10)); // Résultat attendu : true
 console.log(dansPlage(15, 1, 10)); // Résultat attendu : false
+console.log(dansPlage(10, 1, 10)); // Résultat attendu : true
+console.log(dansPlage(-1, -1, 0)); // Résultat attendu : true
+console.log(dansPlage(5, 1, 10)); // Résultat attendu : true
