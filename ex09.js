d6b91088b6b13ee9acaca6f182856f3d72a2f3f8
qwyx <mc010116@gmail.com>
2025-01-23 10:51:07 +0100

    ameliorate 09

--- ex09.js
+++ ex09.js
@@ -1,7 +1,5 @@
 function compterMots(chaine) {
-    for (let i = 0; i < chaine.length; i++) {
-        return chaine.split(" ").length; 
-    }
+    return chaine.split(" ").length; 
 }
 
 console.log(compterMots("Bonjour tout le monde")); // Résultat attendu : 4  
