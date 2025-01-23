15c8cb0905ef20f925939716a54deb7cb36368f3
qwyx <mc010116@gmail.com>
2025-01-23 11:05:10 +0100

    ameliorate 11

--- ex11.js
+++ ex11.js
@@ -1,9 +1,14 @@
 function premiereLettreMajuscule(chaine) {
-    let chaineArray = chaine.split(" ");
-    for (let i = 0; i < chaineArray.length; i++) {
-        chaineArray[i] = chaineArray[i].charAt(0).toUpperCase() + chaineArray[i].slice(1);
+    let mots = chaine.split(" ");
+    let resultat = "";
+    for (let i = 0; i < mots.length; i++) {
+        resultat += mots[i].charAt(0).toUpperCase() + mots[i].slice(1) + " ";
     }
-    return chaineArray.join(" ");
+    return resultat;
 }
 
 console.log(premiereLettreMajuscule("bonjour tout le monde")); // Résultat attendu : "Bonjour Tout Le Monde"
+console.log(premiereLettreMajuscule("Je suis un devellopeur html css js")); // Résultat attendu : Je Suis Un Devellopeur Html Css Js
+console.log(premiereLettreMajuscule("Bonjour tout")); // Résultat attendu : Bonjour Tout
+console.log(premiereLettreMajuscule("Je suis un devellopeur html")); // Résultat attendu : Je Suis Un Devellopeur Html
+console.log(premiereLettreMajuscule("Bonjour je fais du hockey")); // Résultat attendu : Bonjour Je Fais Du Hockey
