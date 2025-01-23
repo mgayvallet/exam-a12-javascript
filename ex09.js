0e19e26992060df1ea80b4b1d0425b9a294f4063
qwyx <mc010116@gmail.com>
2025-01-23 11:02:54 +0100

    ameliorate 09

--- ex09.js
+++ ex09.js
@@ -1,5 +1,10 @@
 function compterMots(chaine) {
-    return chaine.split(" ").length; 
+    let mots = chaine.split(" ");
+    return mots.length;
 }
 
 console.log(compterMots("Bonjour tout le monde")); // Résultat attendu : 4  
+console.log(compterMots("Je suis un devellopeur html css js")); // Résultat attendu : 7
+console.log(compterMots("Bonjour tout")); // Résultat attendu : 2
+console.log(compterMots("Je suis un devellopeur html")); // Résultat attendu : 5
+console.log(compterMots("Bonjour")); // Résultat attendu : 1  
